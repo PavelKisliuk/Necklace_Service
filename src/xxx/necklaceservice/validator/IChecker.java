e078b2c6401be1d52c1e459c68e89d8668ce83eb
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-05-12 11:31:37 +0300

    Commented IChecker

--- src/xxx/necklaceservice/validator/IChecker.java
+++ src/xxx/necklaceservice/validator/IChecker.java
@@ -1,12 +1,62 @@
+/*
+ * By Pavel Kisliuk, 03.05.2019
+ * This is class for education and nothing rights don't reserved.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
 package xxx.necklaceservice.validator;
 
 import xxx.necklaceservice.exception.CustomException;
 
+/**
+ * The {@code IChecker} interface provide method for validation data from
+ * array of {@code String}
+ * @author Kisliuk Pavel Sergeevich
+ * @see StoneChecker
+ * @since 12.0
+ */
 public interface IChecker {
+	/**
+	 * String represent's pattern for regular expression for validation
+	 * value as double
+	 */
 	String UNSIGNED_DOUBLE_PATTERN = "\\d+(\\.\\d+)?";
 
+	/**
+	 * Return {@code true} if all element's of {@param elementsString} are valid,
+	 * else return {@code false}
+	 * @param elementsString is array of {@code String} for validation
+	 * @return {@code true} if all element's of {@param elementsString} are valid
+	 * @throws CustomException if {@param elementsString} contain wrong quantity of element's
+	 * or {@code null}
+	 */
 	boolean isValid(String[] elementsString) throws CustomException;
 
+	/**
+	 * Return {@code true} if {@param unsignedDouble} contain unsigned double,
+	 * else return {@code false}
+	 * @param unsignedDouble is {@code String} for validation
+	 * @return {@code true} if {@param unsignedDouble} contain unsigned double
+	 */
 	default boolean isValidUnsignedDouble(String unsignedDouble) {
 		return unsignedDouble.matches(UNSIGNED_DOUBLE_PATTERN);
 	}
